5ec644a04a6e8f0acd71691a93599e94bae42812
Ruslan Nugmanov <49123257+nugmanov87@users.noreply.github.com>
2019-09-18 13:46:57 +0300

    homeworks #2/version 2

--- functions/main.js
+++ functions/main.js
@@ -43,26 +43,41 @@ function showSolutionsMessage(a, b, c) {
 // 2 задание
 
 function getAverageScore(data) {
-  let object = {
-    algebra: [5, 3, 3, 2],
-    geometry: [4, 1, 5, 4],
-    russian: [5, 5, 3],
-    physics: [3, 4, 5, 5, 5],
-    music: [4, 4, 5, 5],
-    english: [3, 5, 5, 5, 4],
-    poetry: [3, 2, 2, 3],
-    chemistry: [4, 5, 4, 3, 5],
-    french: [3, 5, 5, 5, 4],
-    geografy: [5, 4, 4, 5]
-  };
-
-  for (let prop in object) {
-    let value = object[prop];
-    console.log(`Свойство ${prop}, значение: ${value}`);
+  let averageMarks = {};
+  for (let prop in data) {
+    let value = 0;
+    for (let i = 0; i < data[prop].length; i++) {
+      value += data[prop][i];
+    }
+    averageMarks[prop] = value / data[prop].length;
   }
+  return averageMarks;
 }
 
-getAverageScore();
+function getAverageMark(data) {
+  let averageScores = getAverageScore(data);
+  let sum = 0;
+  let tick = 0;
+  for (let prop in averageScores) {
+    sum += averageScores[prop];
+    tick++;
+  }
+  let average = sum / tick;
+  averageScores.average = average;
+  return averageScores;
+}
+
+console.log(getAverageMark({
+  algebra: [2, 5, 3, 4, 5, 4],
+  geometry: [3, 4, 5],
+  russian: [5, 5, 4, 4],
+  physics: [3, 4],
+  music: [5, 4, 5],
+  english: [4, 4, 3],
+  poetry: [5, 2, 4],
+  chemistry: [3],
+  french: [4, 1]
+}));
 
 // 3 задание
 function getPersonData(secretData) {
