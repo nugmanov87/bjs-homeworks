093eb9c771f4331fac506b7faa3273fd942309db
Ruslan Nugmanov <49123257+nugmanov87@users.noreply.github.com>
2019-09-20 22:55:37 +0500

    homeworks # 4 version 1

--- wrappers-for-primitives/task.js
+++ wrappers-for-primitives/task.js
@@ -1,28 +1,61 @@
+"use strict";
 function calculateMortgage() {
-    let percent = window.percent.value;
-    let contribution = window.contribution.value;
-    let amount = window.amount.value;
-    let date = window.date.value;
+  let percent = window.percent.value;
+  let contribution = window.contribution.value;
+  let amount = window.amount.value;
+  let date = window.date.value;
 
-    let result = calculateTotalMortgage(percent, contribution, amount, date);
-    let span = window.mortageResult;
-    span.textContent = result;
+  let result = calculateTotalMortgage(percent, contribution, amount, date);
+  let span = window.mortageResult;
+  span.textContent = result;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
-
-    // код для задачи №1 писать здесь
-    //return totalAmount;
+  for (let i = 0; i < 3; i++) {
+    if (
+      (typeof arguments[i] != "number" &&
+        typeof Number(arguments[i]) != "number") ||
+      isNaN(Number(arguments[i]))
+    ) {
+      let argName;
+      switch (i) {
+        case 0:
+          argName = "percent";
+          break;
+        case 1:
+          argName = "contribution";
+          break;
+        case 2:
+          argName = "amount";
+          break;
+      }
+      return `Параметр ${argName} содержит неправильное значение ${arguments[i]}`;
+    }
+  }
+  percent /= 100;
+  let p = percent / 12;
+  let nowDate = new Date();
+  let fullTimeMortage = Math.round(
+    (Date.parse(date) - Date.parse(nowDate)) / 1000 / 60 / 60 / 24 / 30
+  );
+  let creditSum = amount - contribution;
+  let paymentMonth = creditSum * (p + p / ((1 + p) ** fullTimeMortage - 1));
+  let totalAmount = paymentMonth * fullTimeMortage;
+  console.log(totalAmount.toFixed(2));
+  return totalAmount.toFixed(2);
 }
 
 function sayHello() {
-    let name = window.personName.value;
-    let greeting = getGreeting(name);
-    let span = window.helloResult;
-    span.textContent = greeting;
+  let name = window.personName.value;
+  let greeting = getGreeting(name);
+  let span = window.helloResult;
+  span.textContent = greeting;
 }
 
 function getGreeting(name) {
-    // код для задачи №2 писать здесь
-    //return greeting;
+  if (isNaN(name)) {
+    return `Привет, мир! Меня зовут ${name} `;
+  } else {
+    return `Привет, мир! Меня зовут Аноним`;
+  }
 }
