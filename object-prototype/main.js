8e44dc3df8fc5b0e3d74f0c06c431864c291b628
Ruslan Nugmanov <49123257+nugmanov87@users.noreply.github.com>
2019-09-22 15:15:29 +0500

    homeworks #5 version 1

--- object-prototype/main.js
+++ object-prototype/main.js
@@ -1,39 +1,57 @@
+"use strict";
 function initCheckBirthday() {
-    const birthday = document.getElementById('birthday').value;
+  const birthday = document.getElementById("birthday").value;
 
-    const result = checkBirthday(birthday) ? "Да" : "Нет";
+  const result = checkBirthday(birthday) ? "Да" : "Нет";
 
-    document.getElementById('disclaimer').innerHTML = result;   
+  document.getElementById("disclaimer").innerHTML = result;
 }
 
 function checkBirthday(birthday) {
-    // код для задачи №1 писать здесь
+  let dateNow = Date.now();
+  let date = new Date(birthday);
+  birthday = +date;
+  let diff = dateNow - birthday;
+  let age = Math.floor((diff / 1000 / 60 / 60 / 24 / (365 * 4 + 1)) * 4);
+  if (age >= 18) {
+    return true;
+  }
 }
 
 function initPrintAnimalSound() {
-    const animal = {
-        sound: 'grrrr',
-    };
+  const animal = {
+    sound: "grrrr"
+  };
 
-    const result = getAnimalSound(animal);
+  const result = getAnimalSound(animal);
 
-    document.getElementById('sound').innerHTML = result;   
+  document.getElementById("sound").innerHTML = result;
 }
 
 function getAnimalSound(animal) {
-    // код для задачи №2 писать здесь
+  if (animal === undefined) {
+    return null;
+  } else {
+    let sound = animal.sound;
+    return sound;
+  }
 }
 
 function initCalculateStatement() {
-    for (let idx = 0; idx < 3; idx++) {
-        const marks = document.getElementById('learner-' + idx).value.split(',');
+  for (let idx = 0; idx < 3; idx++) {
+    const marks = document.getElementById("learner-" + idx).value.split(",");
 
-        const average = getAverageMark(marks);
+    const average = getAverageMark(marks);
 
-        document.getElementById('learner-' + idx + '-average').innerHTML = average;
-    }
+    document.getElementById("learner-" + idx + "-average").innerHTML = average;
+  }
 }
 
 function getAverageMark(marks) {
-    // код для задачи №3 писать здесь
+  let value = 0;
+  for (let i = 0; i < marks.length; i++) {
+    value += Number(marks[i]);
+  }
+  let roundedAverage = value / marks.length;
+  return Math.round(roundedAverage);
 }
